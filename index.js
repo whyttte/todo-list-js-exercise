4a36d3496b9068dec756564ccf638ac0e0645fa9
whyttte <fogboruche@yahoo.com>
2020-08-01 01:17:48 +0000

    methods look better

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function logTaskState(taskIndex) {
 newTask("Clean Cat Litter"); // task 0
 newTask("Do Laundry"); // task 1
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(); // Clean Cat Litter has not been completed
+completeTask();
+logTaskState(); // Clean Cat Litter has been completed
